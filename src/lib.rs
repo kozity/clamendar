082da5370dc9e522d4870f245eb7cfcb92e33330
Ty Kozic <tykozic@protonmail.com>
2021-02-05 23:19:43 -0600

    made good progress on tui layout; simplified backend

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
-use chrono::{DateTime, Duration, Local, Timelike, TimeZone};
+use chrono::{DateTime, Local, Timelike};
 use serde::{Deserialize, Serialize};
 use std::cmp::Ordering;
-use std::io;
 
 const DATE_FORMAT: &str = "%FT%R";
 
@@ -9,17 +8,17 @@ const DATE_FORMAT: &str = "%FT%R";
 pub enum Interval {
     RepDefinite {
         occurrences: usize,
-        duration: Duration,
+        end: DateTime<Local>,
     },
-    RepIndefinite(Duration),
-    Standard(Duration),
+    RepIndefinite(DateTime<Local>),
+    Standard(DateTime<Local>),
     None,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Event {
-    start: Option<DateTime<Local>>,
-    interval: Interval,
+    pub start: Option<DateTime<Local>>,
+    pub interval: Interval,
     pub description: String,
 }
 
@@ -27,15 +26,18 @@ impl Event {
     /// Cycles the start and end times of the interval. Applies only to in/deifnite repeating intervals.
     pub fn advance(&mut self) {
         match self.interval {
-            Interval::RepDefinite { occurrences, duration } => {
-                self.start = Some(self.start.unwrap() + duration);
+            Interval::RepDefinite { occurrences, end } => {
+                let duration = end - self.start.unwrap();
+                self.start = Some(end);
                 self.interval = Interval::RepDefinite {
                     occurrences: occurrences - 1,
-                    duration,
+                    end: end + duration,
                 };
             },
-            Interval::RepIndefinite(duration) => {
-                self.start = Some(self.start.unwrap() + duration);
+            Interval::RepIndefinite(end) => {
+                let start = self.start.unwrap();
+                self.start = Some(end);
+                self.interval = Interval::RepIndefinite(end + (end - start));
             },
             _ => {},
         }
@@ -43,81 +45,11 @@ impl Event {
 
     pub fn end_time(&self) -> Option<String> {
         match self.interval {
-            Interval::RepDefinite { duration, .. }
-                | Interval::RepIndefinite(duration)
-                | Interval::Standard(duration)
-            => Some(
-                (self.start.unwrap() + duration).format(DATE_FORMAT).to_string()
-            ),
+            Interval::RepDefinite { end, .. }
+                | Interval::RepIndefinite(end)
+                | Interval::Standard(end)
+            => Some(end.format(DATE_FORMAT).to_string()),
             Interval::None => None,
-        }
-    }
-
-    // record syntax: "[iso string]\t[description]"
-    fn from_record(line: &str) -> Result<Self, Error> {
-        let mut event = Event {
-            start: None,
-            interval: Interval::None,
-            description: String::new()
-        };
-        let mut parts = line.split('\t');
-        match parts.next() {
-            Some("") => {}, // leave empty fields as assumed.
-            Some(string) => {
-                let mut tokens = string.split('/');
-                match (tokens.next(), tokens.next(), tokens.next()) {
-                    // repeating interval branch.
-                    (Some(repetition), Some(start), Some(end)) => {
-                        // set start date.
-                        event.start = Some(Local.datetime_from_str(start, DATE_FORMAT)?);
-                        // calculate duration.
-                        let duration = Local.datetime_from_str(end, DATE_FORMAT)? - event.start.unwrap();
-                        // calculate occurrences.
-                        let occurrences: Option<usize> = match repetition.strip_prefix('R') {
-                            Some("") => None, // leave occurrences as None.
-                            Some(string) => {
-                                match string.parse::<usize>() {
-                                    Ok(num) => Some(num),
-                                    Err(_) => return Err(Error::InvalidIso),
-                                }
-                            },
-                            None => None, // leave occurrences as None.
-                        };
-                        if let Some(occurrences) = occurrences {
-                            event.interval = Interval::RepDefinite { occurrences, duration };
-                        } else {
-                            event.interval = Interval::RepIndefinite(duration);
-                        }
-                    },
-                    // non-repeating interval branch.
-                    (Some(start), Some(end), None) => {
-                        event.start = Some(Local.datetime_from_str(start, DATE_FORMAT)?);
-                        event.interval = Interval::Standard(Local.datetime_from_str(end, DATE_FORMAT)? - event.start.unwrap());
-                    },
-                    // non-interval branch.
-                    (Some(start), None, None) => {
-                        event.start = Some(Local.datetime_from_str(start, DATE_FORMAT)?);
-                    },
-                    // untimed branch.
-                    (None, None, None) => {},
-                    _ => return Err(Error::InvalidIso),
-                }
-            },
-            None => return Err(Error::InvalidIso),
-        }
-        match parts.next() {
-            Some(string) => event.description.push_str(string),
-            None => {}, // leave the empty string that we've already assumed.
-        }
-        Ok(event)
-    }
-
-    pub fn interval(&self) -> &Interval { &self.interval }
-
-    pub fn is_repeating(&self) -> bool {
-        match self.interval {
-            Interval::RepDefinite{..} | Interval::RepIndefinite(_) => true,
-            _ => false,
         }
     }
 
@@ -139,37 +71,6 @@ impl Event {
                 }
             },
             None => None,
-        }
-    }
-
-    pub fn iso_string(&self) -> String {
-        match (self.start, &self.interval) {
-            (Some(datetime), Interval::RepDefinite { occurrences, duration }) => {
-                format!(
-                    "R{}/{}/{}",
-                    occurrences,
-                    datetime.format(DATE_FORMAT).to_string(),
-                    (datetime + *duration).format(DATE_FORMAT).to_string()
-                )
-            },
-            (Some(datetime), Interval::RepIndefinite(duration)) => {
-                format!(
-                    "R/{}/{}",
-                    datetime.format(DATE_FORMAT).to_string(),
-                    (datetime + *duration).format(DATE_FORMAT).to_string()
-                )
-            },
-            (Some(datetime), Interval::Standard(duration)) => {
-                format!(
-                    "{}/{}",
-                    datetime.format(DATE_FORMAT).to_string(),
-                    (datetime + *duration).format(DATE_FORMAT).to_string()
-                )
-            },
-            (Some(datetime), Interval::None) => {
-                datetime.format(DATE_FORMAT).to_string()
-            },
-            (None, _) => String::new(),
         }
     }
 }
@@ -198,22 +99,3 @@ impl PartialEq<Self> for Event {
 }
 
 impl Eq for Event {}
-
-#[derive(Debug)]
-pub enum Error {
-    ChronoParse(chrono::format::ParseError),
-    InvalidIso,
-    Io(io::Error),
-}
-
-impl From<chrono::format::ParseError> for Error {
-    fn from(error: chrono::format::ParseError) -> Self {
-        Error::ChronoParse(error)
-    }
-}
-
-impl From<io::Error> for Error {
-    fn from(error: io::Error) -> Self {
-        Error::Io(error)
-    }
-}
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,284 @@
+// TODO: replace start_time()'s with functionality native to main.rs
+// TODO: create file if missing
+// TODO: figure out text input in UI
+
 use clamendar::{self, Event, Interval};
-use std::io::{self, BufWriter, Write};
-use std::fs::{self, File};
+use crossterm::{
+    event::{self, KeyCode},
+    terminal,
+};
+use std::io;
+use std::fs;
 use tui::{
     backend::CrosstermBackend,
-    Terminal
+    layout::{Constraint, Direction, Layout},
+    style::{Color, Modifier, Style},
+    Terminal,
+    widgets::{Block, Borders, List, ListItem, ListState, Row, Table, TableState},
 };
-use termion::raw::IntoRawMode;
 
 const FILEPATH: &str = "./events.json";
 
-fn main() -> Result<(), clamendar::Error> {
-    let mut events = deserialize()?;
-    // TODO: define event handling
-    // TODO: initialize terminal
-    // TODO: define layout
-    // TODO: loop
-    loop {
-        break;
+enum Focus {
+    Intervals,
+    Timed,
+    Untimed,
+    None,
+}
+
+struct State {
+    focus: Focus,
+    intervals: TableState,
+    timed: TableState,
+    untimed: ListState,
+}
+
+#[derive(Debug)]
+enum Error {
+    Crossterm(crossterm::ErrorKind),
+    Io(io::Error),
+    Serde(serde_json::Error),
+}
+
+impl From<crossterm::ErrorKind> for Error {
+    fn from(error: crossterm::ErrorKind) -> Self {
+        Error::Crossterm(error)
+    }
+}
+
+impl From<io::Error> for Error {
+    fn from(error: io::Error) -> Self {
+        Error::Io(error)
+    }
+}
+
+impl From<serde_json::Error> for Error {
+    fn from(error: serde_json::Error) -> Self {
+        Error::Serde(error)
+    }
+}
+
+fn main() -> Result<(), Error> {
+    let mut events_intervals: Vec<Event> = Vec::new();
+    let mut events_timed: Vec<Event> = Vec::new();
+    let mut events_untimed: Vec<Event> = Vec::new();
+    for event in deserialize()? {
+        match (&event.start, &event.interval) {
+            (Some(_), Interval::None) => events_timed.push(event),
+            (Some(_), _) => events_intervals.push(event),
+            (None, _) => events_untimed.push(event),
+        }
+    }
+    events_timed.sort_unstable();
+    events_intervals.sort_unstable();
+
+    let mut state = State {
+        focus: Focus::None,
+        intervals: TableState::default(),
+        timed: TableState::default(),
+        untimed: ListState::default(),
+    };
+    if !events_intervals.is_empty() {
+        state.intervals.select(Some(0));
+    }
+    if !events_timed.is_empty() {
+        state.timed.select(Some(0));
+    }
+    if !events_untimed.is_empty() {
+        state.untimed.select(Some(0));
     }
 
-    serialize(events)
+    terminal::enable_raw_mode()?;
+    let mut terminal = Terminal::new(
+        CrosstermBackend::new(
+            io::stdout()
+        )
+    )?;
+    terminal.clear()?;
+
+    loop {
+        terminal.draw(|term| {
+            let main_rectangle = term.size();
+            let chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .constraints(
+                    [
+                        Constraint::Percentage(30),
+                        Constraint::Percentage(70),
+                    ]
+                    .as_ref()
+                )
+                .split(main_rectangle);
+            let chunks_bottom = Layout::default()
+                .direction(Direction::Horizontal)
+                .constraints(
+                    [
+                        Constraint::Percentage(60),
+                        Constraint::Percentage(40),
+                    ]
+                )
+                .split(chunks[1]);
+
+            // Lay out intervals in the top block.
+            let mut table_intervals = Table::new(
+                events_intervals
+                    .iter()
+                    .map(|event| {
+                        Row::new(vec![
+                            event.start_time().unwrap(),
+                            event.description.clone(),
+                        ])
+                    })
+                    .collect::<Vec<Row>>()
+            )
+            .header(Row::new(vec!["End", "Description"])
+                .style(Style::default().add_modifier(Modifier::BOLD))
+            )
+            .block(Block::default()
+                .borders(Borders::ALL)
+                .title("Intervals")
+            )
+            .widths(&[
+                Constraint::Percentage(20),
+                Constraint::Percentage(80),
+            ]);
+
+            // Lay out timed events in the bottom left block.
+            let mut table_timed = Table::new(
+                events_timed
+                    .iter()
+                    .map(|event| {
+                        Row::new(vec![
+                            event.start_time().unwrap(),
+                            event.description.clone(),
+                        ])
+                    })
+                    .collect::<Vec<Row>>()
+            )
+            .header(Row::new(vec!["Time", "Description"])
+                .style(Style::default().add_modifier(Modifier::BOLD))
+            )
+            .block(Block::default()
+                .borders(Borders::ALL)
+                .title("Timed")
+            )
+            .widths(&[
+                Constraint::Percentage(20),
+                Constraint::Percentage(80),
+            ]);
+            
+            // Lay out untimed events in the bottom right block.
+            let mut list_untimed = List::new(events_untimed
+                .iter()
+                .map(|event| ListItem::new(event.description.clone()))
+                .collect::<Vec<ListItem>>()
+            )
+            .block(Block::default()
+                .borders(Borders::ALL)
+                .title("Untimed")
+            );
+
+            match state.focus {
+                Focus::Intervals => {
+                    table_intervals = table_intervals
+                        .highlight_style(Style::default()
+                            .add_modifier(Modifier::BOLD)
+                            .fg(Color::LightBlue)
+                    );
+                },
+                Focus::Timed => {
+                    table_timed = table_timed
+                        .highlight_style(Style::default()
+                            .add_modifier(Modifier::BOLD)
+                            .fg(Color::LightBlue)
+                    );
+                },
+                Focus::Untimed => {
+                    list_untimed = list_untimed
+                        .highlight_style(Style::default()
+                            .add_modifier(Modifier::BOLD)
+                            .fg(Color::LightBlue)
+                    );
+                },
+                Focus::None => {},
+            }
+
+            term.render_stateful_widget(table_intervals, chunks[0], &mut state.intervals);
+            term.render_stateful_widget(table_timed, chunks_bottom[0], &mut state.timed);
+            term.render_stateful_widget(list_untimed, chunks_bottom[1], &mut state.untimed);
+        })?;
+
+        // TODO: handle event
+        match event::read()? {
+            event::Event::Key(keycode) => match keycode.code {
+                KeyCode::Char('g') => state.focus = Focus::Intervals,
+                KeyCode::Char('h') => state.focus = Focus::Timed,
+                KeyCode::Char('j') => {
+                    match state.focus {
+                        Focus::Intervals => {
+                            match state.intervals.selected() {
+                                Some(selected) if selected < events_intervals.len() - 1 => state.intervals.select(Some(selected + 1)),
+                                _ => {},
+                            }
+                        },
+                        Focus::Timed => {
+                            match state.timed.selected() {
+                                Some(selected) if selected < events_timed.len() - 1 => state.timed.select(Some(selected + 1)),
+                                _ => {},
+                            }
+                        },
+                        Focus::Untimed => {
+                            match state.untimed.selected() {
+                                Some(selected) if selected < events_untimed.len() - 1 => state.untimed.select(Some(selected + 1)),
+                                _ => {},
+                            }
+                        },
+                        Focus::None => {},
+                    }
+                },
+                KeyCode::Char('k') => {
+                    match state.focus {
+                        Focus::Intervals => {
+                            match state.intervals.selected() {
+                                Some(selected) if selected > 0 => state.intervals.select(Some(selected - 1)),
+                                _ => {},
+                            }
+                        },
+                        Focus::Timed => {
+                            match state.timed.selected() {
+                                Some(selected) if selected > 0 => state.timed.select(Some(selected - 1)),
+                                _ => {},
+                            }
+                        },
+                        Focus::Untimed => {
+                            match state.untimed.selected() {
+                                Some(selected) if selected > 0 => state.untimed.select(Some(selected - 1)),
+                                _ => {},
+                            }
+                        },
+                        Focus::None => {},
+                    }
+                },
+                KeyCode::Char('l') => state.focus = Focus::Untimed,
+                KeyCode::Char('q') => break,
+                KeyCode::Esc => state.focus = Focus::None,
+                _ => {},
+            },
+            _ => {},
+        }
+    }
+
+    terminal::disable_raw_mode()?;
+    events_timed.append(&mut events_intervals);
+    events_timed.append(&mut events_untimed);
+    serialize(events_timed)
 }
 
-fn deserialize() -> Result<Vec<Event>, clamendar::Error> {
-    let file = fs::read_to_string(FILENAME)?;
-    serde_json::from_str(&file)
+fn deserialize() -> Result<Vec<Event>, Error> {
+    let file = fs::read_to_string(FILEPATH)?;
+    Ok(serde_json::from_str(&file)?)
 }
 
-fn serialize(events: Vec<Event>) -> Result<(), clamendar::Error> {
-    fs::write(FILEPATH, &serde_json::to_vec(&events)?)?
+fn serialize(events: Vec<Event>) -> Result<(), Error> {
+    Ok(fs::write(FILEPATH, &serde_json::to_vec(&events)?)?)
 }
